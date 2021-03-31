f66a2dc740fbfc330542ab29483aa6c8c70a9292
Rennan Cockles <rennancockles@gmail.com>
2021-03-31 14:11:40 -0300

    to_csv helper

--- lojaintegrada_scripts/helpers/__init__.py
+++ lojaintegrada_scripts/helpers/__init__.py
@@ -1,2 +1,3 @@
 from .to_money import to_money
+from .to_csv import to_csv
 from .add_util_days import add_util_days
---
+++ lojaintegrada_scripts/helpers/to_csv.py
@@ -0,0 +1,30 @@
+import csv
+from os import path
+
+CWD = path.dirname(path.abspath(__file__))
+
+def to_csv(data:list[dict]):
+  if not list:
+    return
+
+  keys = data[0].keys()
+  headers = [' '.join([text.title() for text in key.split('_')]) for key in keys]
+
+  file_path = path.join(CWD, 'tmp.csv')
+
+  # valor_total = round(sum([p.valor_total for p in pagamentos]), 2)
+  # valor_taxa = round(sum([p.valor_taxa for p in pagamentos]), 2)
+  # valor_desconto = round(sum([p.valor_desconto for p in pagamentos]), 2)
+  # valor_frete = 0
+  # valor_custo = 0
+  # valor_lucro = 0
+
+  with open(file_path, 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile, dialect='excel')
+    writer.writerow(headers)
+
+    for d in data:
+      row = [d[key] for key in keys]
+      writer.writerow(row)
+
+  return file_path
