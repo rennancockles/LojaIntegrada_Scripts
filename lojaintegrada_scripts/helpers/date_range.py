f00ecccb1cc2efef3a66cd3064d8aafd2ff286e3
Rennan Cockles <rennancockles@gmail.com>
2021-04-01 23:06:08 -0300

    fix date_range to use data instead of datetime

--- lojaintegrada_scripts/helpers/date_range.py
+++ lojaintegrada_scripts/helpers/date_range.py
@@ -1,8 +1,8 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
 
-def date_range(data_i, data_f=None):
+def date_range(data_i, data_f=None)->list[date]:
   if data_f is None:
     data_f = data_i
-  data_i = datetime.strptime(data_i, '%d/%m/%Y').date()
-  data_f = datetime.strptime(data_f, '%d/%m/%Y').date()
-  return [data_i + timedelta(days=x) for x in range((data_f-data_i).days+1)]
+  data_i = datetime.strptime(data_i, '%d/%m/%Y')
+  data_f = datetime.strptime(data_f, '%d/%m/%Y')
+  return [(data_i + timedelta(days=x)).date() for x in range((data_f-data_i).days+1)]
