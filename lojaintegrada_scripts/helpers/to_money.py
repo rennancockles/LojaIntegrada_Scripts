00615e365fff7a4c40a54602d485223612a631d2
Rennan Cockles <rennancockles@gmail.com>
2021-04-02 13:05:37 -0300

    fix to_money empty param

--- lojaintegrada_scripts/helpers/to_money.py
+++ lojaintegrada_scripts/helpers/to_money.py
@@ -1,4 +1,6 @@
 def to_money(valor):
+  if valor is None or valor == '':
+    return ''
   if type(valor) == str:
     valor = float(valor)
   valor_str = 'R$ {0:,.2f}'.format(valor).replace(',', '.')
