4abfa8e699acfe0162a4c1136553dd53808ced55
Rennan Cockles <rennancockles@gmail.com>
2021-05-13 14:06:58 -0300

    change mailer code to send xlsx files

--- lojaintegrada_scripts/helpers/mailer.py
+++ lojaintegrada_scripts/helpers/mailer.py
@@ -2,6 +2,8 @@ import os
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
 
 def send(email_to, subject, body='', files_to_send=tuple()):
   login = os.getenv("GMAIL_LOGIN")
@@ -14,11 +16,20 @@ def send(email_to, subject, body='', files_to_send=tuple()):
   msg["Subject"] = subject
   msg.attach(MIMEText(body, _subtype='plain'))
 
-  for file_hash in files_to_send:
-    with open(file_hash['file'], encoding='utf-8') as fp:
-      attachment = MIMEText(fp.read(), _subtype='csv', _charset='utf-8')
+  for file_dict in files_to_send:
+    file_name = file_dict['file']
+    extension = file_name.rsplit('.', 1)[1]
+    if extension == 'csv':
+      with open(file_name, encoding='utf-8') as fp:
+        attachment = MIMEText(fp.read(), _subtype=extension, _charset='utf-8')
+        attachment.add_header("Content-Disposition", "attachment", filename=f"{file_dict['name']}.{extension}")
+    else:
+      with open(file_name, 'rb') as fp:
+        attachment = MIMEBase('application', "octet-stream")
+        attachment.set_payload(fp.read())
+        encoders.encode_base64(attachment)
+        attachment.add_header("Content-Disposition", "attachment", filename=f"{file_dict['name']}.{extension}")
 
-    attachment.add_header("Content-Disposition", "attachment", filename=f"{file_hash['name']}.csv")
     msg.attach(attachment)
 
   server = smtplib.SMTP("smtp.gmail.com", 587)
