24ba5c48920e45621935a6ab20ee5041a8839cca
Rennan Cockles <rennancockles@gmail.com>
2022-02-09 13:32:10 -0300

    move format dates to helper

--- lojaintegrada_scripts/helpers/__init__.py
+++ lojaintegrada_scripts/helpers/__init__.py
@@ -2,10 +2,11 @@ from .to_money import to_money
 from .to_csv import to_csv
 from .add_util_days import add_util_days
 from .date_range import date_range
+from .format_dates import format_dates
 from .is_pagseguro import is_pagseguro
 from .is_mercadopago import is_mercadopago
 
-month_names = [ 
+month_names = [
   '',
   'Janeiro',
   'Fevereiro',
---
+++ lojaintegrada_scripts/helpers/format_dates.py
@@ -0,0 +1,19 @@
+from datetime import date, datetime, timedelta
+from typing import Optional
+
+from .date_range import date_range
+
+
+def format_dates(
+    date: Optional[str], range: Optional[tuple[str, str]]
+) -> list[date]:
+    yesterday = (datetime.today() - timedelta(days=1)).strftime('%d/%m/%Y')
+    dates = (yesterday, None)
+    if range:
+        dates = range
+    elif date:
+        dates = (date, None)
+
+    dates = date_range(*dates)
+
+    return dates
